6ddd091d9777a207489b970e1a9c643413fa6765
Kamiklza <kamiklza@hotmail.com>
2021-09-09 17:02:49 +0800

    final commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+
+username = os.environ["GMAIL_USERNAME"]
+password = os.environ["GMAIL_PASSWORD"]
 
 response = requests.get(url="https://api.npoint.io/a28614a95d2bdf0666ea")
 data = response.json()
@@ -37,9 +42,20 @@ def home():
 def about_section():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact_section():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        data = request.form
+        connection = smtplib.SMTP("smtp.gmail.com", port=587)
+        connection.starttls()
+        connection.login(user=username, password=password)
+        connection.sendmail(from_addr="kamiklza@gmail.com", to_addrs="kamiklza@hotmail.com", msg=f"Subject: Fans message\n\n Name: {data['name']}\n\n"
+                                                                                                 f"Email: {data['email']}\n\n"
+                                                                                                 f"Phone: {data['phone']}\n\n"
+                                                                                                 f"Message: {data['message']}")
+        connection.close()
+        return render_template('contact.html', msg_sent=True)
+    return render_template('contact.html', msg_sent=False)
 
 @app.route('/post/<int:index>')
 def blog_post(index):
@@ -52,6 +68,10 @@ def blog_post(index):
             date = blog.date
     return render_template('post.html', title=title, subtitle=subtitle, body=body, author=author, date=date)
 
+# @app.route('/form-entry', methods=['POST'])
+# def receive_data():
+#     received = True
+#     return render_template('contact.html', receive=received)
 
 
 if __name__ == "__main__":
